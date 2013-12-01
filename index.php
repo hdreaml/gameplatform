6c515b15473e049425c4ff5c1d1abe38d8eace97
char <hdreaml@126.com>
2013-12-01 21:41:40 +0800

    第一次建立版本

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 13-12-1
+ * Time: ÏÂÎç9:40
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'hello~';
